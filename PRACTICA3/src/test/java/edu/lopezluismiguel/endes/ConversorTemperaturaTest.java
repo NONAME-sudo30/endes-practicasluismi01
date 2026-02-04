38fa81c93f1fa3c178044c59ade17bd6a1312f00
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-04 14:01:25 +0100

    Arreglando errores-v2

---
+++ PRACTICA3/src/test/java/edu/lopezluismiguel/endes/ConversorTemperaturaTest.java
@@ -0,0 +1,24 @@
+package edu.lopezluismiguel.endes;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.* ;
+
+class ConversorTemperaturaTest {
+
+    @Test
+    void testCelsiusAFahrenheit() {
+        Assertions.assertEquals(32.0, ConversorTemperatura.celsiusAFahrenheit(0), 0.0001);
+        assertEquals(212.0, ConversorTemperatura.celsiusAFahrenheit(100), 0.0001);
+        assertEquals(98.6, ConversorTemperatura.celsiusAFahrenheit(37), 0.1);
+    }
+
+    @Test
+    void testFahrenheitACelsius() {
+        assertEquals(0.0, ConversorTemperatura.fahrenheitACelsius(32), 0.0001);
+        assertEquals(100.0, ConversorTemperatura.fahrenheitACelsius(212), 0.0001);
+        assertEquals(37.0, ConversorTemperatura.fahrenheitACelsius(98.6), 0.1);
+    }
+}
+
