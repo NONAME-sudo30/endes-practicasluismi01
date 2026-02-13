8dd55534bb1636fdc80cf13648d47a0e82e8ca85
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-13 09:20:21 +0100

    JavaDoc de la clase Test de la calculadora implementado

--- Practica3Fn/src/test/java/edu/lopezluismiguel/endes/CalculadoraTest.java
+++ Practica3Fn/src/test/java/edu/lopezluismiguel/endes/CalculadoraTest.java
@@ -1,16 +1,23 @@
 
 package edu.lopezluismiguel.endes;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/*Esta clase sirve para realizar
+ pruebas unitarias de funcionamiento en
+  la clase Calculadora sin necesidad de un Main */
 public class CalculadoraTest {
-
+    /* Comprueba que el metodo de la suma devuelva el valor correcto
+     * realizando sumas con valores negativos y positivos*/
     @Test
     public void testSumar() {
         assertEquals(5, Calculadora.sumar(2, 3));
         assertEquals(0, Calculadora.sumar(-2, 2));
     }
+    /* Comprueba que el metodo de la resta devuelva el valor correcto
+     * restando numeros negativos y positivos*/
 
     @Test
     public void testRestar() {
@@ -18,16 +25,21 @@ public class CalculadoraTest {
         assertEquals(-4, Calculadora.restar(-2, 2));
     }
 
+/*Comprueba el correcto funcionamiento de la multiplicación
+* realizando operaciones con distintos numeros enteros */
     @Test
     public void testMultiplicar() {
         assertEquals(6, Calculadora.multiplicar(2, 3));
         assertEquals(-4, Calculadora.multiplicar(-2, 2));
     }
-
+/* Comprueba el funcionamiento correcto de la división
+* realizando operaciones*/
     @Test
     public void testDividir() {
         assertEquals(2, Calculadora.dividir(6, 3));
     }
+/*Comprueba que la división devuelva una excepción
+* al dividir un numero entre 0 */
     @Test
     public void testDividirPorCero() {
         try {
@@ -39,13 +51,6 @@ public class CalculadoraTest {
             System.out.println("No se puede dividir por 0");
         }
     }
-
-
-
-
-
-
-
 
 
 }
