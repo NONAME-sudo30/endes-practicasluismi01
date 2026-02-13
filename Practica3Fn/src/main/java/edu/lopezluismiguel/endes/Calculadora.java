4f524e8a0b80b6fbc2b9521a5c5d7b4207b111c4
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-13 08:57:18 +0100

    Añadido el JavaDoc de la clase calculadora

--- Practica3Fn/src/main/java/edu/lopezluismiguel/endes/Calculadora.java
+++ Practica3Fn/src/main/java/edu/lopezluismiguel/endes/Calculadora.java
@@ -1,27 +1,47 @@
 package edu.lopezluismiguel.endes;
 
 
+/*la clase calculadora en este caso representa distintos metodos
+ de operaciones arimeticas(suma,resta,multiplicación y división)*/
+public class Calculadora {
 
-    public class Calculadora {
-
-        public static int sumar(int a, int b) {
-            return a + b;
-        }
-
-        public static int restar(int a, int b) {
-            return a - b;
-        }
-
-        public static int multiplicar(int a, int b) {
-            return a * b;
-        }
-
-        public static int dividir(int a, int b) {
-            if (b == 0) {
-                throw new ArithmeticException("No se puede dividir por cero");
-            }
-            return a / b;
-        }
-
+    /*Suma de dos valores
+    * parametro (a)
+    * parametro (b)
+    * devuelve la suma de los dos valores */
+    public static int sumar(int a, int b) {
+        return a + b;
     }
+    /* Resta de dos valores
+    * parametro(a)
+    * parametro (b)
+    * devuelve la resta de los valores */
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+    /* Multiplicación de dos valores
+    parametro (a)
+    parametro (b)
+    delvuelve la multiplicación o el producto de dos valores
+     */
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+    /*División de dos valores,en caso de dividir entre 0
+    * se lanzara una ArithmeticException
+    * param (a)
+    * param ( b)
+    * delvuelve el resultado de la división o
+    * en caso de dividir por o, una excepción*/
+
+    public static int dividir(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+
+}
 
