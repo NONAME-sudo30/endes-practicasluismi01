dd295cbd65f4c5e6785c6732018412107e7c6267
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-14 15:50:01 +0100

    Java Doc de la clase ConverseTemperature

--- Practica3Fn/src/main/java/edu/lopezluismiguel/endes/ConverseTemperatura.java
+++ Practica3Fn/src/main/java/edu/lopezluismiguel/endes/ConverseTemperatura.java
@@ -1,10 +1,12 @@
 package edu.lopezluismiguel.endes;
 
-
+/*Esta clase utiliza metodos para cambiar el valor de la temperatura entre celsius y farenheit */
     public class ConverseTemperatura {
+        /* Convertir los valores de temperatura entre celsius y farenheit*/
         public static double celsiusAFahrenheit(double celsius) {
             return (celsius * 9.0 / 5.0) + 32.0;
         }
+        /* Convertir los valores entres farenheit y celsius*/
 
         public static double fahrenheitACelsius(double fahrenheit) {
             return (fahrenheit - 32.0) * 5.0 / 9.0;
