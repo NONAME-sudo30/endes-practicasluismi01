b310d16e8c0a2fdc7ae30e96d4774b6c5c8554cf
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-15 08:53:01 +0100

    JavaDoc de la clase ConverseTemperatureTest

--- Practica3Fn/src/test/java/edu/lopezluismiguel/endes/ConversorTemperaturaTest.java
+++ Practica3Fn/src/test/java/edu/lopezluismiguel/endes/ConversorTemperaturaTest.java
@@ -4,15 +4,17 @@ package edu.lopezluismiguel.endes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.* ;
-
+/* Es un clase que va a testear el correcto funcionamiento
+ de los metodos de la clase ConverseTemperature sin necesidad de un main*/
 class ConversorTemperaturaTest {
-
+/* sirve para testear los metodos  de  Celsius a A Farenheit*/
     @Test
     void testCelsiusAFahrenheit() {
         Assertions.assertEquals(32.0, ConverseTemperatura.celsiusAFahrenheit(0), 0.0001);
         assertEquals(212.0, ConverseTemperatura.celsiusAFahrenheit(100), 0.0001);
         assertEquals(98.6, ConverseTemperatura.celsiusAFahrenheit(37), 0.1);
     }
+    /*Sirve para Testear  los metodos de Farenheit a Celsius */
 
     @Test
     void testFahrenheitACelsius() {
