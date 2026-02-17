612df3933e3a65cd65dd5d51997f9466fc9a0a15
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2026-02-17 20:01:14 +0100

    Java Doc de la clase Validador  completa

--- Practica3Fn/src/main/java/edu/lopezluismiguel/endes/ValidadorPassword.java
+++ Practica3Fn/src/main/java/edu/lopezluismiguel/endes/ValidadorPassword.java
@@ -2,9 +2,9 @@ package edu.lopezluismiguel.endes;
 
 
 import java.util.Scanner;
-
+/*Esta clase utiliza metodo para validar las contraseñas */
     public class ValidadorPassword {
-
+/*Valida la contraseña utilizando distintos metodos */
         public static boolean validar(String password) {
             if (password == null) {
                 return false;
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
             return password.matches(regex);
         }
-
+/* Indica si la contraseña es valida*/
         public static boolean esValida() {
             return false;
         }
--- Practica3Fn/src/test/java/edu/lopezluismiguel/endes/ValidadorPasswordTest.java
+++ Practica3Fn/src/test/java/edu/lopezluismiguel/endes/ValidadorPasswordTest.java
@@ -4,30 +4,32 @@ package edu.lopezluismiguel.endes;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
+/* Es una clase Test que comprueba el funcionamiento de la clase ValidadorPassword*/
     public class ValidadorPasswordTest {
 
-
+/* Comprueba si la contraseña es muy corta*/
         @Test
         public void testPasswordMuyCorta() {
             assertFalse(ValidadorPassword.esValida());
         }
-
+    /* Comprueba si la contraseña no tiene mayusculas*/
         @Test
         public void testSinMayusculas() {
             assertFalse(ValidadorPassword.esValida());
         }
+    /* Comprueba si la contraseña no es minuscula*/
 
         @Test
         public void testSinMinusculas() {
             assertFalse(ValidadorPassword.esValida());
         }
+    /* Comprueba si la contraseña no tiene numero*/
 
         @Test
         public void testSinNumeros() {
             assertFalse(ValidadorPassword.esValida());
         }
-
+    /* Comprueba si la contraseña es nula*/
         @Test
         public void testPasswordNula() {
             assertFalse(ValidadorPassword.esValida());
@@ -37,16 +39,17 @@ import org.junit.jupiter.api.Test;
         public void testValidarNull() {
             assertFalse(false);
         }
-
+    /* Comprueba si la contraseña es correcta*/
         @Test
         public void testValidarCorrecto() {
             assertTrue(ValidadorPassword.validar("Abc123"));
         }
+    /* Comprueba si la contraseña es null*/
         @Test
         public void TestValidarNull() {
             assertFalse(ValidadorPassword.validar(null));
         }
-
+    /* Comprueba si la contraseña es incorrecta*/
         @Test
         public void testValidarIncorrecto() {
             assertFalse(ValidadorPassword.validar("abc"));
